3c96d880abff5329fe27f0b5d45f20ed03807a92
Gusman DP <gusman.dp@gmail.com>
2012-02-02 15:56:52 +0700

    Add pcm audio check

--- player_audio/player_audio.c
+++ player_audio/player_audio.c
@@ -4,6 +4,8 @@
 #include "libavformat/avformat.h"
 #include "libavcodec/avcodec.h"
 
+#include "player_audio_pcm.h"
+
 #define SDL_AUDIO_BUFFER_SIZE 1024
 
 AVFormatContext	*gp_avformat	= NULL;
@@ -12,6 +14,7 @@ AVCodec		*gp_codec	= NULL;
 AVFrame		*gp_decoded_frame = NULL;
 AVPacket	 g_avpacket;
 unsigned char	 g_datalen;
+unsigned char	 g_isdecoding;
 
 void player_audio_init(void)
 {
@@ -88,10 +91,47 @@ bool player_audio_get_codec(void)
 	return TRUE;
 }
 
+unsigned long data_len = 0; 
+AVFrame frame;
+AVPacket packet;
+unsigned char audio_buff[44112] = {0, };
+unsigned int  audio_buff_idx = 0;
 void player_audio_callback(void *userdata, unsigned char *stream, int len)
 {
-	printf("[%s] len: %d\n", __FUNCTION__, len);
+#if 0
+	printf("data_len: %d, len: %d\n", data_len, len);
+	memcpy(stream, (unsigned char*) g_TEST_MODE_SINE5SEC_NORMAL + data_len, len);
+	data_len += len;
+	if (data_len > SINE_5SEC_NORMAL_SIZE)
+		data_len = 0;
+#endif
+#if 1
+	int buffer_size = len;
+	int8_t buffer[len];
+	int get_frame = 0;
 
+	if(0 > av_read_frame(gp_avformat, &packet))
+	{
+		g_isdecoding = 0;
+		printf("Finish decoding\n");
+		return;
+	}    
+
+	if(0 > avcodec_decode_audio4(gp_avcodec, &frame, &get_frame, &packet))
+	{
+		printf("Decoding error\n");
+		return;
+	}
+   
+	if (get_frame)
+	{
+		int data_size;
+
+		data_size = av_samples_get_buffer_size(NULL, gp_avcodec->channels, frame.nb_samples, gp_avcodec->sample_fmt, 1);
+		printf("Data size: %d\n", data_size);
+		memcpy(stream, (unsigned char *) &frame.data[0], data_size);	
+	}
+#endif
 }
 
 bool player_audio_play(void)
@@ -108,9 +148,10 @@ bool player_audio_play(void)
 	desired.freq = gp_avcodec->sample_rate;
 	desired.format = AUDIO_S16SYS;
 	desired.channels = gp_avcodec->channels;
+	desired.silence = 1;
 	desired.samples = SDL_AUDIO_BUFFER_SIZE;
 	desired.callback = player_audio_callback;
-	desired.userdata = NULL;
+	desired.userdata = g_TEST_MODE_SINE5SEC_NORMAL;
 
 	printf("Open audio\n");
 	if(0 > SDL_OpenAudio(&desired, &obtained))
@@ -128,7 +169,8 @@ bool player_audio_play(void)
 	SDL_PauseAudio(0);
 
 	g_datalen = 0xFF;
-	while (g_datalen > 0)
+	g_isdecoding = 1;
+	while (1 == g_isdecoding)
 	{
 		SDL_Delay(100);
 	}
